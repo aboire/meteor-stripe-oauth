7784c091ce32376f771cd3d3711a49efc841e57a
Billy Vong <billy@opstarts.com>
2015-11-10 18:05:07 -0800

    Move files around, clean up package.js

--- package.js
+++ package.js
@@ -1,29 +1,30 @@
 Package.describe({
-    summary: 'Login service for stripe accounts',
-    name: 'accounts-stripe'
+    summary: 'Stripe OAuth Connector',
+    name: 'billyvg:stripe-oauth',
+    version: '0.1.0',
+    git: 'https://github.com/Opstarts/stripe-oauth.git',
 });
 
-Package.onUse( function (api) {
-    api.versionsFrom('1.2'); 
-    if (api.export) api.export('StripeOAuth');     
-    api.use('accounts-base', ['client', 'server']);
-    api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth', ['client', 'server']);
+Package.onUse(function (api) {
+    api.versionsFrom('1.2');
 
-		api.use('oauth', ['client', 'server']);
-    api.use('oauth2', ['client', 'server']);
     api.use('http', ['server']);
-    api.use('underscore', 'server');
     api.use('templating', 'client');
+    api.use('oauth2', ['client', 'server']);
+		api.use('oauth', ['client', 'server']);
     api.use('random', 'client');
+    api.use('underscore', 'server');
     api.use('service-configuration', ['client', 'server']);
-  	
-  	api.addFiles(
-    ['lib/stripe_configure.html', 'lib/stripe_configure.js', 
-    'lib/stripe_login_button.css'],
-    'client');
 
-    api.addFiles("lib/accounts_stripe.js");
-    api.addFiles('lib/stripe_client.js', 'client');
-    api.addFiles('lib/stripe_server.js', 'server');
+    api.export('StripeOAuth');
+
+  	api.addFiles([
+      'lib/stripe_configure.html',
+      'lib/stripe_configure.js',
+      'lib/stripe_login_button.css'
+    ], 'client');
+
+    api.addFiles('stripe_common.js', ['client', 'server']);
+    api.addFiles('stripe_client.js', 'client');
+    api.addFiles('stripe_server.js', 'server');
 });
