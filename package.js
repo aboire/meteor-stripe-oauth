1225478cc23d8b2bdaa838cda3ca8de39f51f709
andy <andy@Goddard.local>
2016-10-20 11:15:04 -0700

    update version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   summary: 'Stripe OAuth Connector',
   name: 'billyvg:stripe-oauth',
-  version: '0.1.2',
+  version: '0.1.4',
   git: 'https://github.com/Opstarts/stripe-oauth.git',
 });
 
